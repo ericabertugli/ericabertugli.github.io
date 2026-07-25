0e8d94e9567d8ce2699a46be42a8015fff236a0a
Erica Bertugli <erica.bertugli@gmail.com>
2026-07-25 17:07:46 +0200

    fix e2e tests for updated skating page title

--- tests/e2e/pages.spec.js
+++ tests/e2e/pages.spec.js
@@ -51,9 +51,9 @@ test.describe("Skating page", () => {
     await page.goto("/pages/skating.html");
 
     await expect(page).toHaveTitle(
-      "Barcelona Inline Skating Routes - Erica Bertugli",
+      "Urban Inline Skating - Erica Bertugli",
     );
-    await expect(page.locator("h1")).toHaveText("Inline Skating");
+    await expect(page.locator("h1")).toHaveText("Urban Inline Skating");
     await expect(page.locator("#map")).toBeVisible();
   });
 
